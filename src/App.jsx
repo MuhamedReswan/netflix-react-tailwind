a83fdea11d105e230dd9ba5ccd36ceac448e4573
MuhamedReswan <muhamedreswan9917@gmail.com>
2024-10-29 12:48:09 +0530

    sign up and sign in page completed with its working

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/account" element={<Account/>} />
       </Routes>
+      </AuthContextProvider>
     </>
   );
 }
